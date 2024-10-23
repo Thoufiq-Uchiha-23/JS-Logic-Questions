59320204c84ac029530663e56c28aa469cc7f513
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-24 00:08:23 +0530

    Promise The Conclusion

--- JavaScript Part 2 -Events, APIs, Async JS, Lexical Scoping, Closures/Promises/promises.js
+++ JavaScript Part 2 -Events, APIs, Async JS, Lexical Scoping, Closures/Promises/promises.js
@@ -53,19 +53,55 @@ promiseFour
   })
   .catch(function (error) {
     console.log(error);
-  }).finally(() => {
-    console.log("The Promise is either resolved or rejected")
   })
+  .finally(() => {
+    console.log("The Promise is either resolved or rejected");
+  });
 
-const promiseFive = new Promise(function(resolve, reject) {
-    setTimeout(() => {
-        let error = false;
-        if (!error) {
-          resolve({ username: "JavaScript", password: "123" });
-        } else {
-          reject("ERROR: JS went wrong!!!");
-        }
-      }, 1000);
-})
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "JavaScript", password: "123" });
+    } else {
+      reject("ERROR: JS went wrong!!!");
+    }
+  }, 1000);
+});
 
 // Now handling Promises using async await
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+//async await with fetch to use as promise
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log("E: ", error);
+//   }
+// }
+
+// getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
