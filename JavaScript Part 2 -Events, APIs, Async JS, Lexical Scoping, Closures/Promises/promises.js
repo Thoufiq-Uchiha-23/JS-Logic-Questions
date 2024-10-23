1f5d104271b017270626199568c5a0ce34871ad8
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-23 23:09:12 +0530

    Promise The Beginning

---
+++ JavaScript Part 2 -Events, APIs, Async JS, Lexical Scoping, Closures/Promises/promises.js
@@ -0,0 +1,71 @@
+const promiseOne = new Promise(function (resolve, reject) {
+  // Promise is used to any Async task
+  // DB Calls, cryptography, network
+  setTimeout(function () {
+    console.log("Async Task Complete");
+    resolve(); // But if we don't add resolve() here, so then() will not be connected
+  }, 1000);
+});
+// then => to handle resolve
+// catch => to handle reject
+promiseOne.then(function () {
+  console.log("Promise Consumed!!!");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve({ username: "Chai", email: "chai@example.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user.username);
+  console.log(user.email);
+});
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = false;
+    if (!error) {
+      resolve({ username: "Thoufiq", password: "123" });
+    } else {
+      reject("ERROR: Something went wrong!!!");
+    }
+  }, 1000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  }).finally(() => {
+    console.log("The Promise is either resolved or rejected")
+  })
+
+const promiseFive = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        let error = false;
+        if (!error) {
+          resolve({ username: "JavaScript", password: "123" });
+        } else {
+          reject("ERROR: JS went wrong!!!");
+        }
+      }, 1000);
+})
+
+// Now handling Promises using async await
