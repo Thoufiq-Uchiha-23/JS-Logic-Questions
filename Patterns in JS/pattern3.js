a9d3e6535328d6d8eab1bb7c84645208da3a6a00
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-26 20:38:13 +0530

    pattern 3

---
+++ Patterns in JS/pattern3.js
@@ -0,0 +1,30 @@
+let count = 1;
+
+for (let i = 1; i <= 4; i++) {
+  for (let j = 1; j <= i; j++) {
+    document.write(count + " ");
+    count++;
+  }
+  document.write("<br>");
+}
+
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+
+
+for(let i=1; i <=4; i++){
+  for(let j=4; j>=i; j++){
+      // document.write("&nbsp;")
+      console.log("&nbsp;")
+  }
+  for(let k=1; k<=i; k++){
+      console.log(i+" ")
+  }
+  console.log("<br>")
+}
+
+console.log("hello")
