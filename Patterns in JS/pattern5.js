3b7e66b13572792bc7ee444adb2eb9c22f1bfe5c
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-26 20:38:29 +0530

    pattern 5

---
+++ Patterns in JS/pattern5.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 4; i++) {
+  for (let j = 4; j >= i; j--) {
+    document.write("&nbsp;");
+  }
+  for (let k = 1; k <= i; k++) {
+    document.write(k + " ");
+  }
+  document.write("<br>");
+}
+
+/*
+    1
+   1 2
+  1 2 3
+ 1 2 3 4
+*/
