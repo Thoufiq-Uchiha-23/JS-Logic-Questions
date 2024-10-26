c8c4f6ed7022c215942e1e95616160c455a26479
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-26 22:47:21 +0530

    star4

---
+++ Patterns in JS/Star Patterns/star4.js
@@ -0,0 +1,23 @@
+// upper Pyramid
+
+let n = 5;
+// External loop
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 1; j <= n - i; j++) {
+    document.write('&nbsp; ')
+  }
+  // printing stars
+  for (let k = 0; k < 2 * i - 1; k++) {
+    document.write('*')
+  }
+  document.write("<br>")
+}
+
+/*
+    *
+   ***
+  *****
+ *******
+*********
+*/
