3d07c98c0ca008e36912c62287bca614ae2d870c
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-26 22:47:28 +0530

    star 5

---
+++ Patterns in JS/Star Patterns/star5.js
@@ -0,0 +1,39 @@
+// Diamond Pyramid
+
+let n = 5;
+
+// Upper half of the diamond
+// Do the upper pyramid as u learnt before
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    document.write("&nbsp; ");
+  }
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
+
+// Lower half of the diamond
+// Here just make the i loop decrement and the rest is same
+for (let i = n - 1; i >= 1; i--) {
+  for (let j = 1; j <= n - i; j++) {
+    document.write("&nbsp; ");
+  }
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
+
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
