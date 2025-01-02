dad7ba425efc14bcd258a5a913bd8051268faa6e
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:10:32 +0530

    star 3

--- Patterns in JS/Star Patterns/star3.js
+++ Patterns in JS/Star Patterns/star3.js
@@ -1,15 +1,15 @@
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  // Print spaces
-  for (let j = 1; j <= n - i; j++) {
-    document.write("&nbsp; ");
-  }
-  // Print stars
-  for (let k = 1; k <= i; k++) {
-    document.write("*");
-  }
-  document.write("<br>");
-}
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   // Print spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     document.write("&nbsp; ");
+//   }
+//   // Print stars
+//   for (let k = 1; k <= i; k++) {
+//     document.write("*");
+//   }
+//   document.write("<br>");
+// }
 
 /*
     *
@@ -18,3 +18,15 @@ for (let i = 1; i <= n; i++) {
  ****
 *****
 */
+
+let n= 5;
+
+for(let i=1; i<=n; i++){
+  for(let j=1; j<=n-i; j++){
+    document.write("&nbsp;")
+  }
+  for(let k=1; k<=i; k++){
+    document.write("* ")
+  }
+  document.write("<br>")
+}
