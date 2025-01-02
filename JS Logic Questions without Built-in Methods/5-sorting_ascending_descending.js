3b31c222fd3311cad0ee9682ced678793429d51c
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 22:02:34 +0530

    ascending descending practice

--- JS Logic Questions without Built-in Methods/5-sorting_ascending_descending.js
+++ JS Logic Questions without Built-in Methods/5-sorting_ascending_descending.js
@@ -1,20 +1,20 @@
 // ASCENDING ORDER
 
 function sortAscending(arr) {
-    let n = arr.length;
+  let n = arr.length;
 
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap arr[j] and arr[j+1]
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
+  }
 
-    return arr;
+  return arr;
 }
 
 // Test case
@@ -22,25 +22,64 @@ console.log(sortAscending([64, 34, 25, 12, 22, 11, 90])); // Output: [11, 12, 22
 
 // DESCENDING ORDER
 function sortDescending(arr) {
-    let n = arr.length;
+  let n = arr.length;
 
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] < arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        // Swap arr[j] and arr[j+1]
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
+  }
 
-    return arr;
+  return arr;
 }
 
 // Test case
 console.log(sortDescending([64, 34, 25, 12, 22, 11, 90])); // Output: [90, 64, 34, 25, 22, 12, 11]
 
 let arr = [3, 5, 2, 8, 1];
-console.log(sortAscending(arr));   // Output: [1, 2, 3, 5, 8]
-console.log(sortDescending(arr));  // Output: [8, 5, 3, 2, 1]
+console.log(sortAscending(arr)); // Output: [1, 2, 3, 5, 8]
+console.log(sortDescending(arr)); // Output: [8, 5, 3, 2, 1]
+
+// PRACTICE
+console.log("Practice");
+
+function ascend(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+function descend(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let array = [3, 5, 2, 8, 1];
+console.log(ascend(array));
+console.log(descend(array));
