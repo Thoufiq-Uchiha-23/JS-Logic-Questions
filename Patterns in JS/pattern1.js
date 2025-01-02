2ded1908aa0d91107e4eab5f7feb40d74a2aa75e
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:09:28 +0530

    pattern 1

--- Patterns in JS/pattern1.js
+++ Patterns in JS/pattern1.js
@@ -1,9 +1,9 @@
-for(let i=1; i <=4; i++){
-    for(let j=1; j<=i; j++){
-        document.write(i +" ")
-    }
-    document.write("<br>")
-}
+// for(let i=1; i <=4; i++){
+//     for(let j=1; j<=i; j++){
+//         document.write(i +" ")
+//     }
+//     document.write("<br>")
+// }
 
 /*
 1
@@ -11,3 +11,10 @@ for(let i=1; i <=4; i++){
 3 3 3
 4 4 4 4
 */
+
+for(let i=1; i<=4; i++){
+    for(let j=1; j<=i; j++){
+        document.write(i+" ")
+    }
+    document.write("<br>")
+}
