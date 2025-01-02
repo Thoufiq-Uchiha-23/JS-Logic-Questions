2b21e0aed95b7cf794dfee0f6bd403cb5409a44b
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:10:08 +0530

    pattern 6

--- Patterns in JS/pattern6.js
+++ Patterns in JS/pattern6.js
@@ -1,14 +1,14 @@
-let count = 1;
-for (let i = 1; i <= 4; i++) {
-  for (let j = 4; j >= i; j--) {
-    document.write("&nbsp;");
-  }
-  for (let k = 1; k <= i; k++) {
-    document.write(count + " ");
-    count++
-  }
-  document.write("<br>");
-}
+// let count = 1;
+// for (let i = 1; i <= 4; i++) {
+//   for (let j = 4; j >= i; j--) {
+//     document.write("&nbsp;");
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     document.write(count + " ");
+//     count++
+//   }
+//   document.write("<br>");
+// }
 
 /*
     1
@@ -16,3 +16,16 @@ for (let i = 1; i <= 4; i++) {
   4 5 6
  7 8 9 10
 */
+
+let count = 1;
+
+for (let i = 1; i <= 4; i++) {
+  for (let j = 4; j >= i; j--) {
+    document.write("&nbsp;");
+  }
+  for (let k = 1; k <= i; k++) {
+    document.write(count + " ");
+    count++;
+  }
+  document.write("<br>");
+}
