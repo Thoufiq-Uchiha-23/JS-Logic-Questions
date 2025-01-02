df00d6fa0ae85e1357aa023f769253942358c09b
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:10:42 +0530

    star 4

--- Patterns in JS/Star Patterns/star4.js
+++ Patterns in JS/Star Patterns/star4.js
@@ -1,18 +1,18 @@
 // upper Pyramid
 
-let n = 5;
-// External loop
-for (let i = 1; i <= n; i++) {
-  // printing spaces
-  for (let j = 1; j <= n - i; j++) {
-    document.write('&nbsp; ')
-  }
-  // printing stars
-  for (let k = 0; k < 2 * i - 1; k++) {
-    document.write('*')
-  }
-  document.write("<br>")
-}
+// let n = 5;
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     document.write('&nbsp; ')
+//   }
+//   // printing stars
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     document.write('*')
+//   }
+//   document.write("<br>")
+// }
 
 /*
     *
@@ -21,3 +21,15 @@ for (let i = 1; i <= n; i++) {
  *******
 *********
 */
+
+let n = 5;
+
+for(let i=1; i<=n; i++){
+  for(let j=1; j<=n-i;j++){
+    document.write("&nbsp ")
+  }
+  for(let k=1; k<= 2 * i - 1; k++){
+    document.write("*")
+  }
+  document.write("<br>")
+}
