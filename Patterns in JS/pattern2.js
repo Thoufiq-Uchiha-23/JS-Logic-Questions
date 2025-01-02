cb7c844d50b329c17aa2e5c98582c794e191caf5
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:09:36 +0530

    pattern 2

--- Patterns in JS/pattern2.js
+++ Patterns in JS/pattern2.js
@@ -1,9 +1,9 @@
-for(let i=1; i <=4; i++){
-    for(let j=1; j<=i; j++){
-        document.write(j +" ")
-    }
-    document.write("<br>")
-}
+// for(let i=1; i <=4; i++){
+//     for(let j=1; j<=i; j++){
+//         document.write(j +" ")
+//     }
+//     document.write("<br>")
+// }
 
 /*
 1
@@ -11,3 +11,10 @@ for(let i=1; i <=4; i++){
 1 2 3
 1 2 3 4
 */
+
+for(let i=1; i<=4; i++) {
+    for(let j=1; j<=i; j++){
+        document.write(j+" ")
+    }
+    document.write("<br>")
+}
