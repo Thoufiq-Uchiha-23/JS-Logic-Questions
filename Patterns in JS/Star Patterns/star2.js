b5e7d11d814b655edf2145f2a5fab396eeedcb89
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:10:23 +0530

    star 2

--- Patterns in JS/Star Patterns/star2.js
+++ Patterns in JS/Star Patterns/star2.js
@@ -1,13 +1,21 @@
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    document.write("* ");
-  }
-  document.write("<br>");
-}
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     document.write("* ");
+//   }
+//   document.write("<br>");
+// }
 
 // * * * * *
 // * * * * *
 // * * * * *
 // * * * * *
 // * * * * *
+
+let n = 5;
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 5; j++) {
+    document.write("* ");
+  }
+  document.write("<br>");
+}
