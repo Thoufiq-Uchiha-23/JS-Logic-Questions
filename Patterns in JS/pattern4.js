408efa4e7fc8defcca176fbe467b97517f60d924
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:09:52 +0530

    pattern 4

--- Patterns in JS/pattern4.js
+++ Patterns in JS/pattern4.js
@@ -1,12 +1,12 @@
-for (let i = 1; i <= 4; i++) {
-  for (let j = 4; j >= i; j--) {
-    document.write("&nbsp;")
-  }
-  for (let k = 1; k <= i; k++) {
-    document.write(i + " ");
-  }
-  document.write("<br>");
-}
+// for (let i = 1; i <= 4; i++) {
+//   for (let j = 4; j >= i; j--) {
+//     document.write("&nbsp;")
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     document.write(i + " ");
+//   }
+//   document.write("<br>");
+// }
 
 /*
     1
@@ -14,3 +14,13 @@ for (let i = 1; i <= 4; i++) {
   3 3 3
  4 4 4 4
 */
+
+for (let i = 1; i <= 4; i++) {
+  for (let j = 4; j >= i; j--) {
+    document.write("&nbsp;");
+  }
+  for (let k = 1; k <= i; k++) {
+    document.write(i + " ");
+  }
+  document.write("<br>");
+}
