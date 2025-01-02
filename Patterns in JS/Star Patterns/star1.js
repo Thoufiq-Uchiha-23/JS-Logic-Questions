832bc93bffd949254311558a7aab6599de2836e3
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-02 23:10:16 +0530

    star 1

--- Patterns in JS/Star Patterns/star1.js
+++ Patterns in JS/Star Patterns/star1.js
@@ -1,12 +1,19 @@
-for (let i = 1; i <= 5; i++) {
-  for (let j = 1; j <= i; j++) {
-    document.write("* ")
-  }
-  document.write("<br>")
-}
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write("* ")
+//   }
+//   document.write("<br>")
+// }
 
 // *
 // * *
 // * * *
 // * * * *
 // * * * * *
+
+for(let i=1; i<=5; i++){
+  for(let j=1; j<=i; j++){
+    document.write("* ")
+  }
+  document.write("<br>")
+}
